29f0b0ac1fd02ab95934e8237481265b07740015
Gr1ffor <54276416+Gr1ffor@users.noreply.github.com>
2021-04-19 09:38:19 -0300

    Navmesh

---
+++ NavMeshStationStarter/Assets/AgentManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+    void Start()
+    {
+        // procura a tag
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    void Update()
+    {
+        // se dar um clique com o botão do mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            // o objeto anda até o local
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                foreach (GameObject a in agents)
+                    a.GetComponent<AiController>().agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
---
+++ NavMeshStationStarter/Assets/AiController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        // chama o componente
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+}
